abfa3b00d56102cbd858c4423823b407bc9ac5fa
Hebert Negron <hjnm194@gmail.com>
2020-02-15 11:46:15 -0600

    Create repunit.c

---
+++ repunit.c
@@ -0,0 +1,25 @@
+//repunit
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	int num,y;
+	bool x=true;
+	printf("Introduzca el numero a evaluar: ");
+	scanf("%i",&num);
+	while(num>0){
+		y=num%10;
+		num=(num-y)/10;
+		if(y!=1){
+			x=false;
+		}
+	}
+	if (x==true){
+		printf("El numero es repunit");
+	}
+	if (x==false){
+		printf("El numero no es repunit");
+	}
+	return 0;
+}
