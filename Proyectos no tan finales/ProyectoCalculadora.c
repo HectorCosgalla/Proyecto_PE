099c296a3e065fc5298cb9add7d61903dd868f89
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-04-19 18:09:59 -0500

    Update ProyectoCalculadora.c

--- Proyectos no tan finales/ProyectoCalculadora.c
+++ Proyectos no tan finales/ProyectoCalculadora.c
@@ -1,33 +1,44 @@
 #include "stdio.h"
 #include <math.h>
+#include "stdbool.h"
 #define MAX 30
+
+//Cadena de prueba: 6-(8^3)#*2+5-7/8^7
 /********** PROTOTIPO DE FUNCIONES **********/
-void comprobSintacticos(char str[]);
+//void comprobLexico(char str[]);
 
 int main(){
   /********** DECLARACION DE VARIABLES **********/
   char str[MAX];
-
+  bool error = false;
   /********** ENTRADAS **********/
   printf("ingresa la cadena prro\n");
   fflush(stdin);
   gets(str);
   /********** PROCESOS **********/
-  comprobSintacticos(str);
+  int i = 0;
+  while (str[i] != '\0' && error == false) {
+    if ((str[i] <= 39 || str[i] == 44 || str[i] == 46 || str[i] >= 58) && str[i] != 94) {
+      printf("\nERROR DE LEXICO EN LA POSICION '%d'(caracter: %c)\n", i+1, str[i]);
+      error = true;
+    }
+    i++;
+  }
   /********** SALIDAS **********/
-  printf("%s\n", str);
+  printf("\n%s\n", str);
   fflush(stdin);
   return 0;
 }
 
-/********** FUNCIONES **********/
-void comprobSintacticos(char str[]) {
+/********** FUNCIONES *********
+void comprobLexico(char str[]) {
   int i = 0;
   while (str[i] != '\0') {
-    if (str[i] <= 39 || str[i] == 44 || str[i] == 46 || str[i] >= 58) {
-      printf("\nERROR SINTACTICO EN %c\n", str[i]);
-
+    if (str[i] <= 39 || str[i] == 44 || str[i] == 46 || str[i] >= 58 && str[i] != 94) {
+      printf("\nERROR DE LEXICO EN LA POSICION '%d'(caracter: %c)\n", i, str[i]);
+      G(3)#*2+5-7/8^7
     }
     i++;
   }
 }
+*/
