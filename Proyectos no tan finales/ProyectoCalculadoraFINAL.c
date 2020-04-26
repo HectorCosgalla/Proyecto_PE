1377e2072ebfecdef52a79316a38dc71983e2997
HectorCosgalla <46679850+HectorCosgalla@users.noreply.github.com>
2020-04-26 18:35:24 -0500

    Update ProyectoCalculadoraFINAL.c

--- Proyectos no tan finales/ProyectoCalculadoraFINAL.c
+++ Proyectos no tan finales/ProyectoCalculadoraFINAL.c
@@ -10,11 +10,11 @@
 
 int main(){
 	/********** DECLARACION DE VARIABLES **********/
-	int parenA = 0, parenC = 0, i = 0, j = 0, k = 0, len;
+	int parenA = 0, parenC = 0, i = 0, j = 0, k = 0, len, aux = 0;
 	char str[MAX];
 	bool error = false;
 	/********** ENTRADAS **********/
-	printf("ingresa la cadena prro\n");
+	printf("ingresa la expresion aritmetica\n");
 	fflush(stdin);
 	gets(str);
 	len = strlen(str)+1;
@@ -51,62 +51,147 @@ int main(){
 		}
 		i++;
 	}
-	i = 0, j = 0;
-	char listaSalida[len], pila[len];
-	for (int i = 0; i < len; i++) {
-		listaSalida[i] = '\0';
-		pila[i] = '\0';
-	}
-	while (str[i] != '\0' && error == false) {
-		switch (str[i]) {
-			case 48:
-			case 49:
-			case 50:
-			case 51:
-			case 52:
-			case 53:
-			case 54:
-			case 55:
-			case 56:
-			case 57:
-				listaSalida[j] = str[i];
-				j++;
-				break;
-			case 40:
-				if (pila[k] != '\0') {
-					pila[k+1] = pila[k];
-					pila[k] = str[i];
-				} else {
-					pila[k] = str[i];
-				}
-				break;
-			case 41:
-				while (pila[k] != '\0' && pila[0] != 40) {
-					listaSalida[j] = pila[k];
-					pila[k] = pila[k+1];
-					k++;
-					j++;
-				}
-				if (pila[0] == 40) {
-					while (pila[k] != '\0') {
-						pila[k] = pila[k+1];
-						k++;
-					}
-				}
-				k=0;
-				break;
-			case 94:
-			case 42:
-			case 47:
-			case 43:
-			case 45:
-				pila[k] = str[i];
-				break;
+	if (error == false) {
+		i = 0, j = 0;
+		char listaSalida[len], pila[len];
+		for (int i = 0; i < len; i++) {
+			listaSalida[i] = '\0';
+			pila[i] = '\0';
 		}
-		printf("%s || %s\n", listaSalida, pila);
-		i++;
+		while (str[i] != '\0') {
+			switch (str[i]) {
+				case 48:
+				case 49:
+				case 50:
+				case 51:
+				case 52:
+				case 53:
+				case 54:
+				case 55:
+				case 56:
+				case 57:
+					listaSalida[j] = str[i];
+					j++;
+					break;
+				case 40:
+					pila[k] = str[i];
+					k++;
+					break;
+				case 41:
+					k--;
+					while (pila[k] != 40) {
+						listaSalida[j] = pila[k];
+						pila[k] = '\0';
+						k--;
+						j++;
+					}
+					if (pila[k] == 40) {
+						pila[k] = '\0';
+					}
+					break;
+				case 94:
+					pila[k] = str[i];
+					k++;
+				case 42:
+				case 47:
+					k--;
+					while (pila[k] != 40 && pila[k] != 94 && pila[k] != 43 && pila[k] != 45 && pila[k] != '\0') {
+						listaSalida[j] = pila[k];
+						pila[k] = '\0';
+						k--;
+						j++;
+					}
+					k++;
+					pila[k] = str[i];
+					k++;
+					break;
+				case 43:
+				case 45:
+					k--;
+					while (pila[k] != 40 && (pila[k] != 94 || pila[k] != 47 || pila[k] != 42) && pila[k] != '\0') {
+						listaSalida[j] = pila[k];
+						pila[k] = '\0';
+						k--;
+						j++;
+					}
+					k++;
+					pila[k] = str[i];
+					k++;
+					break;
+			}
+			i++;
+		}
+		k--;
+		while (pila[k] != '\0') {
+			listaSalida[j] = pila[k];
+			pila[k] = '\0';
+			k--;
+			j++;
+		}
+		i = 0, j = 0, k = 0;
+		int listaSalidaint[MAX];
+		while (listaSalida[i] != '\0') {
+			if (listaSalida[i] < 58 && listaSalida[i] > 47) {
+				switch (listaSalida[i]) {
+					case 48:
+						listaSalidaint[j] = 0;
+						break;
+					case 49:
+						listaSalidaint[j] = 1;
+						break;
+					case 50:
+						listaSalidaint[j] = 2;
+						break;
+					case 51:
+						listaSalidaint[j] = 3;
+						break;
+					case 52:
+						listaSalidaint[j] = 4;
+						break;
+					case 53:
+						listaSalidaint[j] = 5;
+						break;
+					case 54:
+						listaSalidaint[j] = 6;
+						break;
+					case 55:
+						listaSalidaint[j] = 7;
+						break;
+					case 56:
+						listaSalidaint[j] = 8;
+						break;
+					case 57:
+						listaSalidaint[j] = 9;
+						break;
+				}
+				j++;
+			}
+			if (listaSalida[i] == 42 || listaSalida[i] == 43 || listaSalida[i] == 45 || listaSalida[i] == 47 || listaSalida[i] == 94) {
+				j--;
+				switch (listaSalida[i]) {
+					case 42:
+						listaSalidaint[j-1] = listaSalidaint[j-1] * listaSalidaint[j];
+						break;
+					case 43:
+						listaSalidaint[j-1] = listaSalidaint[j-1] + listaSalidaint[j];
+						break;
+					case 45:
+						listaSalidaint[j-1] = listaSalidaint[j-1] - listaSalidaint[j];
+						break;
+					case 47:
+						listaSalidaint[j-1] = listaSalidaint[j-1] / listaSalidaint[j];
+						break;
+					case 94:
+						listaSalidaint[j-1] = pow(listaSalidaint[j-1],listaSalidaint[j]);
+						break;
+				}
+			}
+			i++;
+		}
+		/********** SALIDAS **********/
+		printf("\n");
+		printf("el resultado es: %d\n",listaSalidaint[0]);
+		printf("\n");
 	}
-	/********** SALIDAS **********/
-	printf("%s %s\n", listaSalida, pila);
-	printf("%s\n", str);
+
 }
